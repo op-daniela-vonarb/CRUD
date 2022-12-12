ebc8d3536aaedfc40f742ef21e1a71dc948fdb8e
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-12 16:20:07 +1300

    create update form

--- index.php
+++ index.php
@@ -7,33 +7,39 @@ include 'partials/header.php';
 
 ?>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Website</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($users as $user): ?>
+<div class="container">
+    <table class="table">
+        <thead>
             <tr>
-                <td><?php echo $user['name'] ?></td>
-                <td><?php echo $user['username'] ?></td>
-                <td><?php echo $user['email'] ?></td>
-                <td><?php echo $user['phone'] ?></td>
-                <td><?php echo $user['website'] ?></td>
-                <td>
-                    <a href="view.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-info">View</a>
-                    <a href="update.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-secondary">Update</a>
-                    <a href="delete.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-danger">Delete</a>
-                </td>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Website</th>
+                <th>Actions</th>
             </tr>
-        <?php endforeach;; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user): ?>
+                <tr>
+                    <td><?php echo $user['name'] ?></td>
+                    <td><?php echo $user['username'] ?></td>
+                    <td><?php echo $user['email'] ?></td>
+                    <td><?php echo $user['phone'] ?></td>
+                    <td>
+                        <a target="" href="http://<?php echo $user['website'] ?>">
+                            <?php echo $user['website'] ?>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="view.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-info">View</a>
+                        <a href="update.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-secondary">Update</a>
+                        <a href="delete.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach;; ?>
+        </tbody>
+    </table>
+</div>
     
 <?php include 'partials/footer.php' ?>;
--- view.php
+++ view.php
@@ -19,35 +19,40 @@ if (!$user) {
 
 ?>
 
-
-<div class="card">
-    <div class="card-header">
-        <h3>View User: <b><?php echo $user['name'] ?></b></h3>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>View User: <b><?php echo $user['name'] ?></b></h3>
+        </div>
+        <table class="table">
+            <tbody>
+                <tr>
+                    <th>Name:</th>
+                    <td><?php echo $user['name'] ?></td>
+                </tr>
+                <tr>
+                    <th>Username:</th>
+                    <td><?php echo $user['username'] ?></td>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <td><?php echo $user['email'] ?></td>
+                </tr>
+                <tr>
+                    <th>Phone:</th>
+                    <td><?php echo $user['phone'] ?></td>
+                </tr>
+                <tr>
+                    <th>Website:</th>
+                    <td>
+                        <a target="" href="http://<?php echo $user['website'] ?>">
+                            <?php echo $user['website'] ?>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-    <table class="table">
-        <tbody>
-            <tr>
-                <th>Name:</th>
-                <td><?php echo $user['name'] ?></td>
-            </tr>
-            <tr>
-                <th>Username:</th>
-                <td><?php echo $user['username'] ?></td>
-            </tr>
-            <tr>
-                <th>Email:</th>
-                <td><?php echo $user['email'] ?></td>
-            </tr>
-            <tr>
-                <th>Phone:</th>
-                <td><?php echo $user['phone'] ?></td>
-            </tr>
-            <tr>
-                <th>Website:</th>
-                <td><?php echo $user['website'] ?></td>
-            </tr>
-        </tbody>
-    </table>
 </div>
 
 <?php include 'partials/footer.php' ?>
