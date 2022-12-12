a3a1a2535cf2f8682964372cb9eb48af95b226a0
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-12 16:02:12 +1300

    create card header

--- view.php
+++ view.php
@@ -19,28 +19,35 @@ if (!$user) {
 
 ?>
 
-<table class="table">
-    <tbody>
-        <tr>
-            <th>Name:</th>
-            <td><?php echo $user['name'] ?></td>
-        </tr>
-        <tr>
-            <th>Username:</th>
-            <td><?php echo $user['username'] ?></td>
-        </tr>
-        <tr>
-            <th>Email:</th>
-            <td><?php echo $user['email'] ?></td>
-        </tr>
-        <tr>
-            <th>Phone:</th>
-            <td><?php echo $user['phone'] ?></td>
-        </tr>
-        <tr>
-            <th>Website:</th>
-            <td><?php echo $user['website'] ?></td>
-        </tr>
-    </tbody>
 
-    <?php include 'partials/footer.php' ?>;
+<div class="card">
+    <div class="card-header">
+        <h3>View User: <b><?php echo $user['name'] ?></b></h3>
+    </div>
+    <table class="table">
+        <tbody>
+            <tr>
+                <th>Name:</th>
+                <td><?php echo $user['name'] ?></td>
+            </tr>
+            <tr>
+                <th>Username:</th>
+                <td><?php echo $user['username'] ?></td>
+            </tr>
+            <tr>
+                <th>Email:</th>
+                <td><?php echo $user['email'] ?></td>
+            </tr>
+            <tr>
+                <th>Phone:</th>
+                <td><?php echo $user['phone'] ?></td>
+            </tr>
+            <tr>
+                <th>Website:</th>
+                <td><?php echo $user['website'] ?></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<?php include 'partials/footer.php' ?>
