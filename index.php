d11d2f3cd0ebc0d5c934510a1bc3d9673f57048c
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-12 15:56:29 +1300

    create partials

--- index.php
+++ index.php
@@ -3,19 +3,9 @@ require 'users.php';
 
 $users = getUsers();
 
-?>
+include 'partials/header.php';
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
+?>
 
 <table class="table">
     <thead>
@@ -46,5 +36,4 @@ $users = getUsers();
     </tbody>
 </table>
     
-</body>
-</html>
+<?php include 'partials/footer.php' ?>;
--- view.php
+++ view.php
@@ -1,8 +1,46 @@
 <?php
-
+include 'partials/header.php';
 require 'users.php';
+
+
+if (!isset($_GET['id'])) {
+    include 'partials/not_found.php';
+    exit;
+}
 
 $userId = $_GET['id'];
 
 $user = getUserById($userId);
-print_r($user);
+
+if (!$user) {
+    include 'partials/not_found.php';
+    exit;
+}
+
+?>
+
+<table class="table">
+    <tbody>
+        <tr>
+            <th>Name:</th>
+            <td><?php echo $user['name'] ?></td>
+        </tr>
+        <tr>
+            <th>Username:</th>
+            <td><?php echo $user['username'] ?></td>
+        </tr>
+        <tr>
+            <th>Email:</th>
+            <td><?php echo $user['email'] ?></td>
+        </tr>
+        <tr>
+            <th>Phone:</th>
+            <td><?php echo $user['phone'] ?></td>
+        </tr>
+        <tr>
+            <th>Website:</th>
+            <td><?php echo $user['website'] ?></td>
+        </tr>
+    </tbody>
+
+    <?php include 'partials/footer.php' ?>;
